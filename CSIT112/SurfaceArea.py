491c67ec7a25ad1e04aa78872aac7ecdf05f3493
Teresa Miller <teresawithoutah@gmail.com>
2018-04-13 13:59:30 -0400

    soph hw

---
+++ CSIT112/SurfaceArea.py
@@ -0,0 +1,9 @@
+l = float(input("Enter integer for LENGTH of rectangular prism: "))
+w = float(input("Enter integer for WIDTH of rectangular prism: "))
+h = float(input("Enter integer for HEIGHT of rectangular prism: "))
+
+surface_area = (2 * l * w + 2 * (l + w) * h)
+
+print("Surface Area:", round(surface_area, 2), "square feet")
+
+
